aa8d435858fce093ebf1ea4f848fa7a2bf36359e
Tommy Truong <ttruong@campaignmonitor.com>
2023-09-15 14:38:55 +1000

    Implement large progress bar.

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,31 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const ProgressMeter = styled.div`
+    background-color: hsla(0, 0%, 50%, 0.15);
+    border-radius: 8px;
+    box-shadow: 0 2px 4px 0 hsla(0, 0%, 50%, 0.35) inset;
+    display: flex;
+    height: 24px;
+    padding: 4px;
+    width: 370px;
+  `;
+  const ProgressMeterFill = styled.svg`
+    background-color: hsla(240, 80%, 60%, 1);
+    border-radius: ${(props) => props.value !== 100 ? '4px 0 0 4px' : '4px'};
+    width: ${(props) => props.value + '%'}
+  `;
+
+  return (
+      <ProgressMeter
+          role="progressbar"
+          aria-valuenow={value}
+          aria-valuemin="0"
+          aria-valuemax="100"
+      >
+        <ProgressMeterFill value={value}/>
+      </ProgressMeter>
+  );
 };
 
 export default ProgressBar;
