5a51a7854f89b31f9adc44c191c86362fe3e5825
Tommy Truong <ttruong@campaignmonitor.com>
2023-09-26 09:37:22 +1000

    Make changes such that focus and hover work.

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -8,50 +8,66 @@ import { getDisplayedValue } from './Select.helpers';
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
+  const Wrapper = styled.div`
+    position: relative;
+    width: max-content;
+  `;
+  const NativeSelect = styled.select`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    /* Allow the select to span the full height in Safari */
+    -webkit-appearance: none;
+  `;
+
   const SelectBox = styled.div`
-    align-items: center;
-    display: flex;
     background-color: ${COLORS.transparentGray35};
-    border: 2px solid transparent;
     border-radius: 8px;
     color: ${COLORS.gray700};
-    padding: 12px 16px;
-    position: relative;
-    text-align: center;
-    width: fit-content;
+    font-size: 1rem;
+    padding: 12px 52px 12px 16px;
     
-    & > :not(:first-child) {
-      padding-left: 24px;
-    }
-    
-    &:focus {
-      border-color: ${COLORS.primary};
+    & > * {
+      display: inline-block;
     }
 
-    &:hover {
+    ${NativeSelect}:hover + & {
       color: ${COLORS.black};
     }
-    
-    & select {
-      opacity: 0;
-      height: 100%;
-      left: 0;
-      position: absolute;
-      top: 0;
-      width: 100%;
+
+    ${NativeSelect}:focus + & {
+      outline: 1px dotted #212121;
+      outline: 5px auto -webkit-focus-ring-color;
     }
   `;
 
+  const IconWrapper = styled.div`
+    bottom: 0;
+    height: var(--size);
+    position: absolute;
+    margin: auto;
+    top: 0;
+    right: 10px;
+    width: var(--size)
+  `;
+
   return (
-      <SelectBox aria-label={label}>
-        <p>
-           {displayedValue}
-        </p>
-        <Icon id="chevron-down"/>
-        <select value={value} onChange={onChange}>
+      <Wrapper>
+        <NativeSelect value={value} onChange={onChange}>
           {children}
-        </select>
-      </SelectBox>
+        </NativeSelect>
+        <SelectBox aria-label={label}>
+          <p>
+             {displayedValue}
+          </p>
+          <IconWrapper style={{'--size': 24 + 'px'}}>
+            <Icon id="chevron-down" strokeWidth="2" size="24"/>
+          </IconWrapper>
+        </SelectBox>
+      </Wrapper>
   );
 };
 
